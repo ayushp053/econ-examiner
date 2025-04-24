ffef619455857572468e713b0f1502f4843109f8
ayushp053 <ayushpatel053@gmail.com>
2025-04-24 18:02:03 +0100

    Update textarea.tsx

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,1 +1,8 @@
-// TODO: Add code here
+export function Textarea({ ...props }) {
+  return (
+    <textarea
+      className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+      {...props}
+    />
+  );
+}
