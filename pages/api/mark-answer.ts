62f31127d7cd5fc0982126be82b757979cee7da5
ayushp053 <ayushpatel053@gmail.com>
2025-04-24 17:47:32 +0100

    Update mark-answer.ts

--- pages/api/mark-answer.ts
+++ pages/api/mark-answer.ts
@@ -1,1 +1,11 @@
+const rawContent = textResponse.choices[0].message.content || '';
+const jsonStart = rawContent.indexOf('[');
+const jsonEnd = rawContent.lastIndexOf(']') + 1;
+
+let feedback = [];
+try {
+  feedback = JSON.parse(rawContent.slice(jsonStart, jsonEnd));
+} catch (err) {
+  console.error('Failed to parse feedback JSON:', err);
+}
 // TODO: Add code here
