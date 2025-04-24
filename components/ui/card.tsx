5b8398f002d4cd0141b9bd52ee1d0c868f6dd98f
ayushp053 <ayushpatel053@gmail.com>
2025-04-24 18:01:45 +0100

    Update card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,1 +1,9 @@
-// TODO: Add code here
+export function Card({ children }) {
+  return (
+    <div className="border border-gray-200 rounded-xl shadow-sm bg-white">{children}</div>
+  );
+}
+
+export function CardContent({ children, className = "" }) {
+  return <div className={`p-4 ${className}`}>{children}</div>;
+}
