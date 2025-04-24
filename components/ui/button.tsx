404a236f882105da5e3df29b9c81f76c8761079f
ayushp053 <ayushpatel053@gmail.com>
2025-04-24 18:01:24 +0100

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,1 +1,10 @@
-// TODO: Add code here
+export function Button({ children, ...props }) {
+  return (
+    <button
+      className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition font-semibold"
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
